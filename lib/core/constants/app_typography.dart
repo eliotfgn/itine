00718ac4914d70154f25235910b52634def23144
Eliot Fagnon <efagnon2002@gmail.com>
2023-09-14 16:00:40 +0100

    feat: add app typography call to register all common typography styles

---
+++ lib/core/constants/app_typography.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:itine/core/constants/app_colors.dart';
+
+class AppTypography {
+  static const headline1 = TextStyle(
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w500,
+    fontSize: 26,
+  );
+
+  static const headline4 = TextStyle(
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w400,
+    fontSize: 17,
+  );
+
+  static const subtitle2 = TextStyle(
+    color: Colors.black45,
+    fontSize: 17,
+  );
+
+  static const hint = TextStyle(
+    color: Colors.black38,
+    fontSize: 14,
+  );
+}
