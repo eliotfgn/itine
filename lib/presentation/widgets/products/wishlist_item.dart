318082d487f8f74c3dd5fb6d2af7c500c71d0612
Eliot Fagnon <efagnon2002@gmail.com>
2023-09-18 15:34:08 +0100

    feat: add dismissible behavior

--- lib/presentation/widgets/products/wishlist_item.dart
+++ lib/presentation/widgets/products/wishlist_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:itine/core/constants/app_colors.dart';
 import 'package:itine/core/constants/app_typography.dart';
@@ -22,83 +23,103 @@ class WishlistItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.15),
-              spreadRadius: 3,
-              blurRadius: 5,
-              offset: const Offset(-3, 4), // changes position of shadow
+    return Dismissible(
+      key: Key(product.id ?? ''),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.secondary,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/trash.svg',
+              height: 30,
             ),
           ],
-          borderRadius: BorderRadius.circular(10)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            height: 85,
-            width: 85,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: AppColors.primary,
-              image: DecorationImage(
-                  image: AssetImage("assets/images/${product.cover}"),
-                  fit: BoxFit.cover),
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.15),
+                spreadRadius: 3,
+                blurRadius: 5,
+                offset: const Offset(-3, 4), // changes position of shadow
+              ),
+            ],
+            borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 85,
+              width: 85,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: AppColors.primary,
+                image: DecorationImage(
+                    image: AssetImage("assets/images/${product.cover}"),
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          SizedBox(
-            height: 85,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: Get.width * 0.30,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        product.name,
-                        style: AppTypography.headline3.copyWith(fontSize: 17),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        'Itine France',
-                        style: AppTypography.subtitle2.copyWith(fontSize: 15),
-                      ),
-                    ],
+            const SizedBox(
+              width: 15,
+            ),
+            SizedBox(
+              height: 85,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: Get.width * 0.30,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.name,
+                          style: AppTypography.headline3.copyWith(fontSize: 17),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Itine France',
+                          style: AppTypography.subtitle2.copyWith(fontSize: 15),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const Spacer(),
-                Text(
-                  '${product.price} €',
-                  style: AppTypography.headline3.copyWith(
-                    fontWeight: FontWeight.w700,
+                  const Spacer(),
+                  Text(
+                    '${product.price} €',
+                    style: AppTypography.headline3.copyWith(
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          CustomButton(
-            onTap: () {},
-            width: 125,
-            vPadding: 5,
-            color: AppColors.secondary,
-            child: const Text(
-              'Ajouter au panier',
-              style: TextStyle(fontSize: 11),
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
+            CustomButton(
+              onTap: () {},
+              width: 125,
+              vPadding: 5,
+              color: AppColors.secondary,
+              child: const Text(
+                'Ajouter au panier',
+                style: TextStyle(fontSize: 11),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
