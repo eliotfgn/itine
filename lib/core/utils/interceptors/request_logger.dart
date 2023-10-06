994adf4ac186e1145fbbaba2558b60f47833eb91
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-07 00:40:33 +0100

    feat: add request logger interceptor

---
+++ lib/core/utils/interceptors/request_logger.dart
@@ -0,0 +1,56 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart' as getx;
+import 'package:itine/presentation/controllers/request/request_controller.dart';
+
+class Logger extends Interceptor {
+  final RequestController _requestController =
+      getx.Get.find<RequestController>();
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    log('--------------------------------------------------');
+    log('[DIO REQUEST]: ${options.method} ${options.baseUrl + options.path}');
+    log('[REQUEST AUTHORIZATION]: ${options.headers['Authorization']}');
+    log('--------------------------------------------------');
+
+    _requestController.isLoading.toggle();
+
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    log('--------------------------------------------------');
+    log('[DIO RESPONSE] ${response.statusCode} => PATH: ${response.requestOptions.path}');
+    log('--------------------------------------------------');
+
+    _requestController.isLoading.toggle();
+
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    log('--------------------------------------------------');
+    log('[DIO ERROR] ${err.response?.statusCode.toString()} => PATH: ${err.requestOptions.path}');
+    log(err.response?.data['message'] ?? 'An unhandled error occured.');
+    getx.Get.showSnackbar(
+      getx.GetSnackBar(
+        message: err.response?.data['message'] ?? 'Une erreur est survenue.',
+        borderRadius: 50,
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.red.shade300,
+        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      ),
+    );
+
+    _requestController.isLoading.toggle();
+
+    log('--------------------------------------------------');
+
+    super.onError(err, handler);
+  }
+}
