c48b5c556b114f53229c620151229faab441ddc7
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-07 18:06:35 +0100

    feat: add flow for adding items to cart

--- lib/presentation/controllers/product/products_controller.dart
+++ lib/presentation/controllers/product/products_controller.dart
@@ -1,10 +1,16 @@
 import 'package:get/get.dart';
 import 'package:itine/core/services/api/products/product_service.dart';
+import 'package:itine/domains/models/cart_item/cart_item.dart';
 import 'package:itine/domains/models/product/product.dart';
+import 'package:itine/presentation/controllers/product/cart_controller.dart';
 
 class ProductsController extends GetxController {
   final ProductService _productService = ProductService();
+  final CartController _cartController = CartController();
 
+  RxInt quantity = 0.obs;
+  RxString selectedColor = ''.obs;
+  RxString selectedSize = ''.obs;
   RxList<Product> products = <Product>[].obs;
 
   @override
@@ -15,5 +21,22 @@ class ProductsController extends GetxController {
 
   fetchProducts() async {
     products.value = await _productService.getAll();
+  }
+
+  addToCart(Product product) {
+    CartItem item = CartItem(
+      product: product,
+      quantity: quantity.value,
+      color: selectedColor.value,
+      size: selectedSize.value,
+    );
+
+    _cartController.addToCart(item);
+    resetSelection();
+  }
+
+  resetSelection() {
+    selectedSize.value = '';
+    selectedColor.value = '';
   }
 }
