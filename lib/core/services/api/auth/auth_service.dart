9b5b91f66f5ac1d97b6a238322f443f5cc13e1f9
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-21 17:08:52 +0100

    fat: Add login service

--- lib/core/services/api/auth/auth_service.dart
+++ lib/core/services/api/auth/auth_service.dart
@@ -2,12 +2,20 @@ import 'package:dio/dio.dart';
 import 'package:itine/core/services/api/api_endpoints.dart';
 import 'package:itine/core/services/api/base_api_service.dart';
 import 'package:itine/domains/models/user/user_request.dart';
+import 'package:itine/presentation/routes/app_routes.dart';
 
 class AuthService extends ApiService {
   register(UserRequest payload) async {
     Response response =
-    await client.post(ApiEndpoints.register, data: payload.toJson());
+        await client.post(ApiEndpoints.register, data: payload.toJson());
 
     return response.statusCode == 200;
+  }
+
+  login(String email, String password) async {
+    Response response = await client.post(AppRoutes.login, data: {
+      'email': email,
+      'password': password,
+    });
   }
 }
