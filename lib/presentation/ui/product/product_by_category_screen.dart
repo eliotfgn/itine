4d2e8585e4657a24166afc8a140ee13a638b430b
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-21 23:02:38 +0100

    fix: products list loading and refresh

--- lib/presentation/ui/product/product_by_category_screen.dart
+++ lib/presentation/ui/product/product_by_category_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:itine/domains/models/category/category.dart';
 import 'package:itine/presentation/controllers/product/products_controller.dart';
+import 'package:itine/presentation/controllers/request/request_controller.dart';
 import 'package:itine/presentation/widgets/products/product_card.dart';
 
 import '../../widgets/common/CustomTextFormField.dart';
@@ -16,14 +17,19 @@ class ProductByCategoryScreen extends StatefulWidget {
 }
 
 class _ProductByCategoryScreenState extends State<ProductByCategoryScreen> {
+  final RequestController _requestController = Get.find<RequestController>();
   final ProductsController _productsController = Get.find<ProductsController>();
   final TextEditingController searchController = TextEditingController();
 
   Category category = Get.arguments;
 
+  init() async {
+    await _productsController.fetchProductsByCategoryAndGender(category.name);
+  }
+
   @override
   void initState() {
-    _productsController.fetchProductsByCategoryAndGender(category.name);
+    init();
     super.initState();
   }
 
@@ -33,33 +39,40 @@ class _ProductByCategoryScreenState extends State<ProductByCategoryScreen> {
       body: SafeArea(
         child: Stack(
           children: [
-            ListView(
-              physics: const BouncingScrollPhysics(),
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
-              children: [
-                const SizedBox(
-                  height: 65,
-                ),
-                CustomTextFormField(
-                  hint: 'Rechercher',
-                  visible: true,
-                  controller: searchController,
-                  leading: const Icon(Icons.search),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Wrap(
-                  spacing: 10,
-                  runSpacing: 20,
-                  alignment: WrapAlignment.spaceBetween,
-                  children: [
-                    ..._productsController.products.map(
-                          (product) => ProductCard(product: product),
-                    )
-                  ],
-                )
-              ],
+            Obx(
+              () => ListView(
+                physics: const BouncingScrollPhysics(),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+                children: [
+                  const SizedBox(
+                    height: 65,
+                  ),
+                  CustomTextFormField(
+                    hint: 'Rechercher',
+                    visible: true,
+                    controller: searchController,
+                    leading: const Icon(Icons.search),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  _requestController.isLoading.isTrue
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : Wrap(
+                          spacing: 10,
+                          runSpacing: 20,
+                          alignment: WrapAlignment.spaceBetween,
+                          children: [
+                            ..._productsController.products.map(
+                              (product) => ProductCard(product: product),
+                            )
+                          ],
+                        )
+                ],
+              ),
             ),
             CustomBackButton(
               onTap: () {
