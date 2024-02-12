c09174ef7a5ec946468d77ed6e0cdb4fd5477944
Eliot Fagnon <efagnon2002@gmail.com>
2024-02-12 16:21:15 +0100

    feat: Updates

--- lib/presentation/controllers/product/wishlist_controller.dart
+++ lib/presentation/controllers/product/wishlist_controller.dart
@@ -5,5 +5,11 @@ class WishlistController extends GetxController {
   RxInt index = 0.obs;
   RxList<WishlistItem> items = <WishlistItem>[].obs;
 
+  @override
+  void onInit() async {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
   void addToWishlist() {}
 }
