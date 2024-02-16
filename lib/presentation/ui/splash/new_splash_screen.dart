3a442d2765353d5b032cd4edd55aedad1c72a93c
Giovani <88268809+YounGio9@users.noreply.github.com>
2024-02-16 11:13:11 +0100

    Update new_splash_screen.dart

--- lib/presentation/ui/splash/new_splash_screen.dart
+++ lib/presentation/ui/splash/new_splash_screen.dart
@@ -33,6 +33,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     checkIsFirstAppLoad();
+    loading = false;
   }
 
   @override
