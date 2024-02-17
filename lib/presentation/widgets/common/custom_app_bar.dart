441af855efbee2c6b86b4e8d5be07e37677366d1
Eliot Fagnon <efagnon2002@gmail.com>
2024-02-17 11:54:22 +0100

    feat: Add page name on category page

---
+++ lib/presentation/widgets/common/custom_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:itine/core/constants/app_typography.dart';
+import 'package:itine/presentation/widgets/common/back_button.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key, required this.title, this.onReturn});
+
+  final String title;
+  final VoidCallback? onReturn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          CustomBackButton(
+            onTap: () {
+              if (onReturn != null) {
+                onReturn!();
+              } else {
+                Get.back();
+              }
+            },
+          ),
+          const SizedBox(
+            width: 40,
+          ),
+          Text(
+            title,
+            style: AppTypography.headline2,
+          ),
+        ],
+      ),
+    );
+  }
+}
