9532e9e4f5b24e7650eaf214f0e6ae8170d3a381
Eliot Fagnon <efagnon2002@gmail.com>
2024-02-19 17:47:25 +0100

    fix: Remove back button from wishlist screen

--- lib/presentation/ui/root_navigation/wishlist_screen.dart
+++ lib/presentation/ui/root_navigation/wishlist_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:itine/core/constants/app_typography.dart';
 import 'package:itine/domains/models/product/product.dart';
 import 'package:itine/presentation/widgets/common/back_button.dart';
+import 'package:itine/presentation/widgets/common/custom_app_bar.dart';
 import 'package:itine/presentation/widgets/products/wishlist_item.dart';
 
 class WishlistScreen extends StatelessWidget {
@@ -12,26 +13,18 @@ class WishlistScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Stack(
+      child: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
         children: [
-          ListView(
-            physics: const BouncingScrollPhysics(),
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            children: [
-              const SizedBox(
-                height: 65,
-              ),
-              Text(
-                'Ma liste de souhait',
-                style: AppTypography.headline1.copyWith(fontSize: 25),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              ...products.map((product) => WishlistItem(product: product))
-            ],
+          Text(
+            'Ma liste de souhait',
+            style: AppTypography.headline1.copyWith(fontSize: 25),
           ),
-          const CustomBackButton(),
+          const SizedBox(
+            height: 25,
+          ),
+          ...products.map((product) => WishlistItem(product: product))
         ],
       ),
     );
